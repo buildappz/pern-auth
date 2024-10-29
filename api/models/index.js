b4e9cc1756608753fedf54c185cfec0b74bfe88b
Eddy Valverde <eddygarros@hotmail.com>
2024-10-28 22:05:42 -0600

    fix: typo

--- api/models/index.js
+++ api/models/index.js
@@ -19,8 +19,8 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.user = require("./user.model.js")(sequelize, Sequelize);
-db.role = require("./role.model.js")(sequelize, Sequelize);
+db.user = require("./user.models.js")(sequelize, Sequelize);
+db.role = require("./role.models.js")(sequelize, Sequelize);
 
 db.role.belongsToMany(db.user, {
   through: "user_roles"
